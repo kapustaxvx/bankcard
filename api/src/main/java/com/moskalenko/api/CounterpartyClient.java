5438d6750acad3ff4fd2dd6d04615613fce2bee9
a19190480 <IIMoskalenko@sberbank.ru>
2021-05-24 09:57:58 +0300

    add documentation to interface

--- api/src/main/java/com/moskalenko/api/CounterpartyClient.java
+++ api/src/main/java/com/moskalenko/api/CounterpartyClient.java
@@ -8,14 +8,42 @@ import java.util.Collection;
 
 public interface CounterpartyClient {
 
+    /**
+     * Создание контрагента
+     * @param request информация о контрагенте(название компании)
+     * @return контрагент
+     */
     Counterparty createCounterparty(CounterpartyDescriptionRequest request);
 
+    /**
+     * Получение баланса
+     *
+     * @param accountId аккаунт лицевого счета
+     * @return баланс
+     */
     BigDecimal getBalance(Long accountId);
 
+    /**
+     * Запрос на пополнение баланса
+     *
+     * @param accountId аккаунт лицевого счета
+     * @param amount сумма пополнения
+     */
     void increaseBalance(Long accountId, BigDecimal amount);
 
+    /**
+     * Запрос на списание баланса
+     *
+     * @param accountId аккаунт лицевого счета
+     * @param amount сумма списания
+     */
     void decreaseBalance(Long accountId, BigDecimal amount);
 
+    /**
+     * Список всех контрагентов
+     *
+     * @return список контрагентов
+     */
     Collection<Counterparty> getCounterparties();
 
 }
--- api/src/main/java/com/moskalenko/api/CustomerClient.java
+++ api/src/main/java/com/moskalenko/api/CustomerClient.java
@@ -7,14 +7,44 @@ import java.util.Collection;
 
 public interface CustomerClient {
 
+    /**
+     * Создание банковской карты для клиента
+     *
+     * @param accountId аккаунт лицевого счета
+     * @return банковская карта
+     */
     BankCard createBankCard(Long accountId);
 
+    /**
+     * Список всех банковских карт клиента
+     *
+     * @param customerId клиент
+     * @return список карт
+     */
     Collection<BankCard> getBankCards(Long customerId);
 
+    /**
+     * Запрос на пополнение баланса
+     *
+     * @param cardId карта клиента
+     * @param amount сумма пополнения
+     */
     void increaseBalance(Long cardId, BigDecimal amount);
 
+    /**
+     * Запрос на списание с баланса
+     *
+     * @param cardId карта клиента
+     * @param amount сумма списания
+     */
     void decreaseBalance(Long cardId, BigDecimal amount);
 
+    /**
+     * Запрос на получение баланса
+     *
+     * @param accountId аккаунт лицевого счета
+     * @return баланс
+     */
     BigDecimal getBalance(Long accountId);
 
 }
